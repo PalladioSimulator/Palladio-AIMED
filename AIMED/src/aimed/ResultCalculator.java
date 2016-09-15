c452b1e818801e41224706c4915dbee5641372e2
MarcelTUC <marcel.mueller@s2013.tu-chemnitz.de>
2016-09-15 12:13:39 +0200

    Further implementations due to response time calculation

--- AIMED/src/aimed/ResultCalculator.java
+++ AIMED/src/aimed/ResultCalculator.java
@@ -3,9 +3,11 @@ package aimed;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.TreeMap;
 
 import javax.measure.quantity.Duration;
 import javax.measure.quantity.Frequency;
@@ -36,6 +38,7 @@ public class ResultCalculator {
 	private Amount<Frequency> throughputCpu = Amount.valueOf(0, SI.HERTZ);
 	private Amount<Frequency> throughputHdd = Amount.valueOf(0, SI.HERTZ);
 	private Map<InternalAction, Set<Amount<Duration>>> responseTimesPerInternalAction;
+	private Amount<Duration> prevTimeStamp;
 
 	public ResultCalculator() {
 		responseTimesPerInternalAction = new HashMap<>();
@@ -58,15 +61,10 @@ public class ResultCalculator {
 		splitMeasurementData(methodName, data.getRecords());
 		ResourceDemandingSEFF seff = fileProcessor.getSeff(methodName);
 		Map<InternalAction, ResourceDemandingInterval> intervals = getResourceDemandingIntervals(seff);
+		prevTimeStamp = Amount.valueOf(0, SI.NANO(SI.SECOND));
 		for (InternalAction ia : intervals.keySet()) {
 			calculateResponseTimes(ia, intervals.get(ia));
 		}
-		
-		/*Amount<Duration> methodResponseTime;
-		for (List<ResponseTimeRecord> records : measurementsList) {
-			methodResponseTime = calculateResponseTime(seff, records);
-			System.out.println(methodName + ": " + methodResponseTime.doubleValue(SI.SECOND));
-		}*/
 	}
 	
 	private void splitMeasurementData(String methodName, List<AbstractRecord> measurementRecords) {
@@ -77,6 +75,7 @@ public class ResultCalculator {
 			rtRec = (ResponseTimeRecord) record;
 			oneMeasurement.add(rtRec);
 			if (LpeStringUtils.patternMatches(rtRec.getOperation(), methodName)) {
+				//TODO: Add only if is trace 1 method.
 				measurementsList.add(oneMeasurement);
 				oneMeasurement = new ArrayList<>();
 			}
@@ -84,12 +83,11 @@ public class ResultCalculator {
 	}
 	
 	private Map<InternalAction, ResourceDemandingInterval> getResourceDemandingIntervals(ResourceDemandingSEFF seff) {
-		Map<InternalAction, ResourceDemandingInterval> result = new HashMap<>();
+		Map<InternalAction, ResourceDemandingInterval> result = new LinkedHashMap<>();
 		List<AbstractAction> actions = seff.getSteps_Behaviour();
 		ResourceDemandingInterval interval;
 		for (AbstractAction action : actions) {
 			if (action instanceof InternalAction) {
-				result = new HashMap<>();
 				interval = getIntervalOfInternalAction((InternalAction) action);
 				result.put((InternalAction) action, interval);
 			}
@@ -157,9 +155,76 @@ public class ResultCalculator {
 		return null;
 	}
 	
-
 	private void calculateResponseTimes(InternalAction ia, ResourceDemandingInterval rdi) {
-		
+		Set<Amount<Duration>> responseTimes = new HashSet<>();
+		for (List<ResponseTimeRecord> records : measurementsList) {
+			responseTimes.add(getResponseTimePerRecord(rdi, records));	
+		}
+		responseTimesPerInternalAction.put(ia, responseTimes);
+	}
+	
+	private Amount<Duration> getResponseTimePerRecord(ResourceDemandingInterval rdi, List<ResponseTimeRecord> records) {
+		Amount<Duration> beginTimeStamp = Amount.valueOf(0, SI.NANO(SI.SECOND));
+		Amount<Duration> endTimeStamp = Amount.valueOf(0, SI.NANO(SI.SECOND));
+		if (rdi.end instanceof ExternalCallAction) {
+			endTimeStamp = getMethodNanoTimestamp(getMethodDefinitionAndAddPlaceholder((ExternalCallAction) rdi.end), records, prevTimeStamp);
+		}
+		if (rdi.end instanceof StopAction) {
+			endTimeStamp = getMethodNanoTimestamp(methodName, records).plus(getResponseTimeByThisTimeStamp(getMethodNanoTimestamp(methodName, records), records));
+		}
+		if (rdi.begin instanceof StartAction) {
+			beginTimeStamp = getMethodNanoTimestamp(methodName, records);
+		}
+		if (rdi.begin instanceof ExternalCallAction) {
+			String method = getMethodDefinitionAndAddPlaceholder((ExternalCallAction) rdi.begin);
+			beginTimeStamp = getMethodNanoTimestamp(method, records, prevTimeStamp);
+			if (wasInALoop((ExternalCallAction) rdi.begin)) {
+				beginTimeStamp = getLastExternalCallTimeStamp(method, beginTimeStamp, endTimeStamp, records);
+			}
+			beginTimeStamp = beginTimeStamp.plus(getResponseTimeByThisTimeStamp(beginTimeStamp, records));
+		}
+		Amount<Duration> result = endTimeStamp.minus(beginTimeStamp);
+		NanoResponseTimeRecord nsRec;
+		for (ResponseTimeRecord record : records) {
+			nsRec = (NanoResponseTimeRecord) record;
+			if (nsRec.getNanoTimestamp() > beginTimeStamp.getExactValue() &&
+					nsRec.getNanoTimestamp() < endTimeStamp.getExactValue()) {
+				result = result.minus(Amount.valueOf(nsRec.getResponseTime(), SI.NANO(SI.SECOND)));
+			}
+		}
+		//prevTimeStamp = endTimeStamp;
+		return result;
+	}
+	
+	private boolean wasInALoop (ExternalCallAction eca){
+		List<AbstractAction> actions = fileProcessor.getSeff(methodName).getSteps_Behaviour();
+		for(AbstractAction action : actions) {
+			if (action instanceof LoopAction) {
+				LoopAction la = (LoopAction) action;
+				List<AbstractAction> loopActions = la.getBodyBehaviour_Loop().getSteps_Behaviour();
+				for (AbstractAction loopAction : loopActions) {
+					if (loopAction.equals(eca)) {
+						return true;
+					}
+				}
+			}
+		}
+		return false;
+	}
+	
+	private Amount<Duration> getLastExternalCallTimeStamp(String methodPattern, Amount<Duration> beginTimeStamp, Amount<Duration> endTimeStamp, List<ResponseTimeRecord> records) {
+		Amount<Duration> prevNanoTimeStamp = Amount.valueOf(0, SI.NANO(SI.SECOND));
+		NanoResponseTimeRecord nRec;
+		for (ResponseTimeRecord rec : records) {
+			if (LpeStringUtils.patternMatches(rec.getOperation(), methodPattern)) {
+				nRec = (NanoResponseTimeRecord) rec;
+				if (nRec.getNanoTimestamp() >= beginTimeStamp.getExactValue() &&
+						nRec.getNanoTimestamp() < endTimeStamp.getExactValue()) {
+					prevNanoTimeStamp = Amount.valueOf(nRec.getNanoTimestamp(), SI.NANO(SI.SECOND));
+				}
+			}
+ 		}		
+		return prevNanoTimeStamp;
 	}
 	
 	private Amount<Duration> getResponseTimeByThisTimeStamp(Amount<Duration> nanoTimeStamp, List<ResponseTimeRecord> records) {
@@ -188,28 +253,22 @@ public class ResultCalculator {
 		return result;
 	}
 
-	private Amount<Duration> getLoopActionBodyExternalCallTimestamp (LoopAction la, List<ResponseTimeRecord> records){
-		List<AbstractAction> actions = la.getBodyBehaviour_Loop().getSteps_Behaviour();
-		for (AbstractAction action : actions) {
-			if (action instanceof ExternalCallAction) {
-				ExternalCallAction eca = (ExternalCallAction) action;
-				System.out.println(eca.getEntityName());
-				return getMethodNanoTimestamp(getMethodDefinitionAndAddPlaceholder(eca), records);
-			}
-		}
-		return Amount.valueOf(0, SI.NANO(SI.SECOND));
-	}
-		
-	private Amount<Duration> getMethodNanoTimestamp(String methodName, List<ResponseTimeRecord> records) {
+	private Amount<Duration> getMethodNanoTimestamp(String methodName, List<ResponseTimeRecord> records, Amount<Duration> startAtTimeStamp) {
 		NanoResponseTimeRecord nr;
 		for (ResponseTimeRecord record : records) {
 			if (LpeStringUtils.patternMatches(record.getOperation(), methodName)) {
 				nr = (NanoResponseTimeRecord) record;
-				return Amount.valueOf(nr.getNanoTimestamp(), SI.NANO(SI.SECOND));
+				if (nr.getNanoTimestamp() >= startAtTimeStamp.getExactValue()) {
+					return Amount.valueOf(nr.getNanoTimestamp(), SI.NANO(SI.SECOND));
+				}
 			}
 		}
 		System.out.println("Method not found in getMethodNanoTimestamp");
 		return Amount.valueOf(0, SI.NANO(SI.SECOND));
+	}
+	
+	private Amount<Duration> getMethodNanoTimestamp(String methodName, List<ResponseTimeRecord> records) {
+		return getMethodNanoTimestamp(methodName, records, Amount.valueOf(0, SI.NANO(SI.SECOND)));
 	}
 	
 	private String getMethodDefinitionAndAddPlaceholder(ExternalCallAction eca) {
